643de00912ffe0d01475ffe044261c3a29b3d68e
hikaru1224 <130636591+tuna2142@users.noreply.github.com>
2023-05-25 14:00:14 +0900

    ボタン追加

--- v0525/Form1.cs
+++ v0525/Form1.cs
@@ -6,5 +6,10 @@ namespace v0525
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
     }
 }
